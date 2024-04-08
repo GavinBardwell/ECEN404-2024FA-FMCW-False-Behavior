27a227734441eead0d424e86d52817c82b1e4c89
Gavin Bardwell <gmbwe110@gmail.com>
2024-04-07 21:22:02 -0500

    hello

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import './App.css';
 import { useState } from 'react' /*used for updating the state of an object*/
+import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Routes, Route, BrowserRouter, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
-import Navbar from './Navbar';
+import Navbar from './assets/Navbar';
+import RunOverview from './assets/Overview';
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar/>
+        <div className="App-window">
         <Routes>
-        <Route index element={<index />}></Route>
+          <Route index element={<RunOverview/>}></Route>
         </Routes>
+        </div>
       </Router>
-      <header className="App-header">
-        <p>hello world</p>
-      </header>
     </div>
   );
 }
