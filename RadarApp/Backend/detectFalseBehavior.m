69ee101770f4f0d67a72ff8883c950821212e2d9
Gavin Bardwell <93444087+GavinBardwell@users.noreply.github.com>
2024-11-07 08:47:21 -0600

    noise done... detection implemented

--- RadarApp/Backend/detectFalseBehavior.m
+++ RadarApp/Backend/detectFalseBehavior.m
@@ -1,6 +1,12 @@
-function isFalse = detectFalseBehavior(simData)
-%USER CODE GOES HERE
-
-%%USER CODE GOES HERE
+function isFakeBehavior = detectFalseBehavior(response, rng_grid, dop_grid)
+    %INFO
+        %response -- post processed FFT informaiton. Is a complex MxP
+        %matrix. M is the range measurements and P is the doppler
+        %measurements
+        %rng_grid -- range samples that FFT was evaluated... of length M
+        %dop_grid -- velocity samples that FFT was evaluated... of length P
+    %USER CODE GOES HERE
+    isFakeBehavior = false();
+    %%USER CODE GOES HERE
 end
 
